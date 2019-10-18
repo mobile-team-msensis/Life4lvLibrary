c0a68090f0260e195a1c88412db6829a2c4c5554
Amoutzidis <d.amoutzidis@msensis.com>
2019-10-18 15:44:26 +0300

    Added splash screen to have view model

--- library/src/main/java/com/msensis/life4lv/library/ui/splash/SplashActivity.kt
+++ library/src/main/java/com/msensis/life4lv/library/ui/splash/SplashActivity.kt
@@ -3,19 +3,27 @@ package com.msensis.life4lv.library.ui.splash
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.msensis.life4lv.library.R
 import com.msensis.life4lv.library.ui.prototypes.BaseActivity
+import com.msensis.life4lv.library.ui.prototypes.BaseViewModel
+import com.msensis.life4lv.library.ui.prototypes.MVVMActivity
 
-open class SplashActivity: BaseActivity()
+open class SplashActivity<VM: BaseViewModel, VB: ViewDataBinding>: MVVMActivity<VM, VB>()
 {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
     }
 
-    private fun nextActivity(clazz: Class<*>) {
+    override fun getViewModelClass(): Class<VM> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getLayoutId() = R.layout.activity_splash
+
+    protected fun nextActivity(clazz: Class<*>) {
 
         startActivity(Intent(this, clazz))
         finish()
